6cdac36c229aa862dadaf1e639da26b8800c4411
H4wk <quasand89@gmail.com>
2022-09-19 12:48:36 +0200

    add colors to the board

--- src/main.js
+++ src/main.js
@@ -19,8 +19,6 @@ const scores = {
   TIE: 0,
 };
 
-let meStart = false;
-
 const Color = Object.freeze({
   GREEN: "rgb(1, 121, 51)",
   YELLOW: "rgb(204, 112, 0)",
@@ -36,6 +34,7 @@ let gameMode = gameModes.vsAI;
 let userChoice = choices[0];
 let enemyChoice = choices[1];
 let currChoice = choices[1];
+let meStart = userChoice === "O";
 
 function fillBoard(field) {
   /**
@@ -45,6 +44,7 @@ function fillBoard(field) {
    */
 
   document.getElementById(field).innerText = currChoice;
+  document.getElementById(field).style.backgroundColor = Color.RED;
 
   const idx = fields.indexOf(field);
   if (idx !== -1) {
@@ -85,7 +85,7 @@ function displayResultText() {
 
   if (!hasEnded()) return;
 
-  const gameState = getGameState();
+  const gameState = getGameState(true);
   if (gameState === enemyChoice) {
     document.querySelector(".result").innerText = `${enemyChoice} wins!`;
   } else if (gameState === userChoice) {
@@ -95,10 +95,11 @@ function displayResultText() {
   }
 }
 
-function getGameState() {
+function getGameState(color = false) {
   /**
    * If the game has ended, return the winner.
    *
+   * @param {Boolean} color if set to true, color the winning fields green.
    * @return {String} winner: 'X, 'O', 'TIE' or "" if no winner.
    */
 
@@ -108,8 +109,16 @@ function getGameState() {
       board[i] !== "" &&
       board[i] === board[i + 3] &&
       board[i + 3] === board[i + 6]
-    )
+    ) {
+      if (color) {
+        document.getElementById(fields[i]).style.backgroundColor = Color.GREEN;
+        document.getElementById(fields[i + 3]).style.backgroundColor =
+          Color.GREEN;
+        document.getElementById(fields[i + 6]).style.backgroundColor =
+          Color.GREEN;
+      }
       return board[i];
+    }
   }
 
   /* horizontal */
@@ -118,17 +127,37 @@ function getGameState() {
       board[i] !== "" &&
       board[i] === board[i + 1] &&
       board[i + 1] === board[i + 2]
-    )
+    ) {
+      if (color) {
+        document.getElementById(fields[i]).style.backgroundColor = Color.GREEN;
+        document.getElementById(fields[i + 1]).style.backgroundColor =
+          Color.GREEN;
+        document.getElementById(fields[i + 2]).style.backgroundColor =
+          Color.GREEN;
+      }
       return board[i];
+    }
   }
 
   /* main diagonal */
-  if (board[0] !== "" && board[0] === board[4] && board[4] === board[8])
+  if (board[0] !== "" && board[0] === board[4] && board[4] === board[8]) {
+    if (color) {
+      document.getElementById(fields[0]).style.backgroundColor = Color.GREEN;
+      document.getElementById(fields[4]).style.backgroundColor = Color.GREEN;
+      document.getElementById(fields[8]).style.backgroundColor = Color.GREEN;
+    }
     return board[0];
+  }
 
   /* second diagonal */
-  if (board[2] !== "" && board[2] === board[4] && board[4] === board[6])
+  if (board[2] !== "" && board[2] === board[4] && board[4] === board[6]) {
+    if (color) {
+      document.getElementById(fields[2]).style.backgroundColor = Color.GREEN;
+      document.getElementById(fields[4]).style.backgroundColor = Color.GREEN;
+      document.getElementById(fields[6]).style.backgroundColor = Color.GREEN;
+    }
     return board[2];
+  }
 
   if (hasTied()) return "TIE";
 
@@ -171,11 +200,9 @@ function getBestMove() {
    * @return {void}
    */
 
-  currChoice = enemyChoice;
   const isMaximizing = enemyChoice === "X";
   let bestScore = isMaximizing ? -Infinity : Infinity;
   let bestMoveIdx = -1;
-
   for (let i = 0; i < 9; i++) {
     // if the field is taken, continue
     if (board[i] !== "") continue;
@@ -201,6 +228,8 @@ function getBestMove() {
   // after checking every field, place the mark on the optimal position
   board[bestMoveIdx] = enemyChoice;
   document.getElementById(fields[bestMoveIdx]).innerText = enemyChoice;
+  document.getElementById(fields[bestMoveIdx]).style.backgroundColor =
+    Color.RED;
   changeMark();
 }
 
@@ -264,7 +293,11 @@ function playVsAI() {
 
 function resetBoard() {
   board.fill("");
-  fields.forEach((field) => (document.getElementById(field).innerText = ""));
+  fields.forEach((field) => {
+    document.getElementById(field).innerText = "";
+    document.getElementById(field).style.backgroundColor = "";
+  });
+
   currChoice = "O";
 
   // generate best AI move
