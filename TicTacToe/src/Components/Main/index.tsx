11571e38eed0a06d306b2a1e08ef5941fe5c10ee
H4wk <quasand89@gmail.com>
2023-06-16 23:25:29 +0200

    vsAi mode, change sides and restart button

--- TicTacToe/src/Components/Main/index.tsx
+++ TicTacToe/src/Components/Main/index.tsx
@@ -1,43 +1,23 @@
 import { useState } from "react";
 import styles from "./style.module.scss";
 
-const choices = ["X", "O"] as const;
-const scores = {
-  X: 1,
-  O: -1,
-  TIE: 0,
-} as const;
+//const Color = Object.freeze({
+//  // other color formats do not work with comparison.
+//  GREEN: "rgb(1, 121, 51)",
+//  YELLOW: "rgb(204, 125, 0)",
+//  RED: "rgb(130, 0, 0)",
+//});
 
-const Color = Object.freeze({
-  // other color formats do not work with comparison.
-  GREEN: "rgb(1, 121, 51)",
-  YELLOW: "rgb(204, 125, 0)",
-  RED: "rgb(130, 0, 0)",
-});
+type Mode = "vsAI" | "vsFriend";
+type Choice = "X" | "O";
+type Square = Choice | null;
+type Board = Square[];
 
-const gameModes = Object.freeze({
-  vsAI: Symbol("vsAI"),
-  vsFriend: Symbol("vsFriend"),
-});
+const getEmptyBoard = (): Board => new Array(9).fill(null);
 
-// default settings
-let gameMode = gameModes.vsAI;
-let userChoice = choices[0];
-let enemyChoice = choices[1];
-let currChoice: "O" | "X" = choices[1];
-let meStart = true;
+const hasTied = (board: Board): boolean => !board.includes(null);
 
-type Choice = "X" | "O" | ""; // TODO: change "" to null
-type Board = Choice[];
-
-const hasTied = (board: Board) => {
-  for (const field of board) {
-    if (field === "") return false;
-  }
-  return true;
-};
-
-const getWinner = (board: Board) => {
+const getWinner = (board: Board): Choice | null => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -49,94 +29,220 @@ const getWinner = (board: Board) => {
     [2, 4, 6],
   ];
   for (const [a, b, c] of lines) {
-    if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
       return board[a];
     }
   }
   return null;
 };
 
-const hasEnded = (board: Board) => hasTied(board) || getWinner(board) !== null;
+const hasEnded = (board: Board): boolean =>
+  hasTied(board) || getWinner(board) !== null;
 
-const changeMark = () => {
-  const idx = choices.indexOf(currChoice);
-  currChoice = choices[idx ^ 1];
+function getBestMove(
+  board: Board,
+  setBoard: (board: Board) => void,
+  currChoice: Choice,
+) {
+  /**
+   * Check every possible available field, calculate value of the field,
+   * and set the mark on the highest value field.
+   */
+
+  const enemyChoice = currChoice === "O" ? "X" : "O";
+  const isMaximizing = enemyChoice === "X";
+  const fakeBoard = [...board];
+  let bestScore = isMaximizing ? -Infinity : Infinity;
+  let bestMoveIdx: number | null = null;
+  for (let i = 0; i < 9; i++) {
+    // if the field is taken, continue
+    if (fakeBoard[i]) continue;
+
+    // place the mark on the board
+    fakeBoard[i] = enemyChoice;
+    /* calculate the value of that placement, assuming the other player
+       is playing optimally. */
+    const currScore = minimax(!isMaximizing, fakeBoard);
+    // remove the mark from the board
+    fakeBoard[i] = null;
+
+    // if optimal placement was found, save it and get its index
+    if (
+      (enemyChoice === "O" && currScore < bestScore) ||
+      (enemyChoice === "X" && currScore > bestScore)
+    ) {
+      bestScore = currScore;
+      bestMoveIdx = i;
+    }
+  }
+
+  // after checking every field, place the mark on the optimal position
+  if (bestMoveIdx !== null) {
+    setBoard(board.map((cell, i) => (i === bestMoveIdx ? enemyChoice : cell)));
+  }
+  // document.getElementById(fields[bestMoveIdx]).innerText = enemyChoice;
+  // document.getElementById(fields[bestMoveIdx]).style.background = Color.RED;
+}
+
+function minimax(isMaximizing: boolean, board: Board) {
+  if (hasEnded(board)) {
+    const result = getWinner(board);
+    if (!result) return 0; // TIE
+    return result === "X" ? 1 : -1;
+  }
+
+  let bestScore = isMaximizing ? -Infinity : Infinity;
+  const choice = isMaximizing ? "X" : "O";
+  for (let i = 0; i < 9; i++) {
+    // if the field is taken, continue
+    if (board[i]) continue;
+
+    board[i] = choice;
+    const score = minimax(!isMaximizing, board);
+    board[i] = null;
+    bestScore = isMaximizing
+      ? Math.max(bestScore, score)
+      : Math.min(bestScore, score);
+  }
+
+  return bestScore;
+}
+
+const placeMark = (
+  idx: number,
+  board: Board,
+  setBoard: (board: Board) => void,
+  currChoice: Choice,
+  setCurrChoice: (currChoice: Choice) => void,
+  mode: Mode,
+): void => {
+  if (hasEnded(board) || board[idx]) return;
+  const newBoard = board.map((cell, i) => (i === idx ? currChoice : cell));
+  setBoard(newBoard);
+  if (mode === "vsAI") {
+    getBestMove(newBoard, setBoard, currChoice);
+  } else {
+    setCurrChoice(currChoice === "O" ? "X" : "O");
+  }
 };
 
-const game = (idx: number, board: Board, setBoard: (board: Board) => void) => {
-  if (!hasEnded(board)) {
-    // if the field is already occupied, return
-    if (board[idx] !== "") return;
-
-    // place user's mark
-    setBoard(
-      board.map((cell, i) => {
-        if (i === idx) return currChoice;
-        return cell;
-      }),
-    );
-    changeMark();
-    //if (gameMode === gameModes.vsAI) {
-    //  vsFriend.style.background = Color.RED;
-    //} else {
-    //  vsAi.style.background = Color.RED;
-    //}
-    //changeSidesBtn.style.background = Color.RED;
-    // place enemy's mark
-    //if (!hasEnded() && gameMode === gameModes.vsAI) {
-    //  getBestMove();
-    //}
-    // say that it is now user's turn
-    //result.innerText = `${currChoice} turn`;
-  }
-  //displayResultText();
+const getResultText = (board: Board): string => {
+  const winner = getWinner(board);
+  return winner ? `${winner} wins!` : "Tie!";
 };
 
 export default function Main() {
-  const [board, setBoard] = useState<Board>([
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ]);
+  const [board, setBoard] = useState<Board>(getEmptyBoard());
+  const [mode, setMode] = useState<Mode>("vsFriend");
+  const [currChoice, setCurrChoice] = useState<Choice>("O");
+
+  const resultText = hasEnded(board)
+    ? getResultText(board)
+    : `${currChoice} turn`;
 
   return (
     <div className={styles["center-container"]}>
       <div className={styles.main}>
         <div className={styles.board}>
           {board.map((cell, i) => (
-            <Box content={cell} idx={i} board={board} setBoard={setBoard} />
+            <Box
+              key={i}
+              content={cell}
+              idx={i}
+              board={board}
+              setBoard={setBoard}
+              currChoice={currChoice}
+              setCurrChoice={setCurrChoice}
+              mode={mode}
+            />
           ))}
         </div>
         <div className={styles["result-container"]}>
-          <div className={styles.result}>X turn</div>
+          <div className={styles.result}>{resultText}</div>
         </div>
       </div>
       <div className={styles.right}>
-        <button className={styles["vs-friend"]}>Play vs friend</button>
-        <button className={styles["vs-ai"]}>Play vs AI</button>
-        <button className={styles["change-sides"]}>Change sides</button>
-        <button className={styles.restart}>Restart</button>
+        <button
+          onClick={() => {
+            setMode("vsFriend");
+            setBoard(getEmptyBoard());
+          }}
+          className={`${mode === "vsFriend" ? styles.active : styles.inactive}`}
+        >
+          Play vs friend
+        </button>
+        <button
+          onClick={() => {
+            setMode("vsAI");
+            const newBoard = getEmptyBoard();
+            setBoard(newBoard);
+            //setCurrChoice("X");
+            if (currChoice === "X") {
+              getBestMove(newBoard, setBoard, currChoice);
+            }
+          }}
+          className={`${mode === "vsAI" ? styles.active : styles.inactive}`}
+        >
+          Play vs AI
+        </button>
+        <button
+          className={styles.inactive}
+          onClick={() => {
+            const newBoard = getEmptyBoard();
+            const newChoice = currChoice === "O" ? "X" : "O";
+            setBoard(newBoard);
+            setCurrChoice(newChoice);
+            if (mode === "vsAI" && newChoice === "X") {
+              getBestMove(newBoard, setBoard, newChoice);
+            }
+          }}
+        >
+          Change sides
+        </button>
+        <button
+          className={styles.inactive}
+          onClick={() => {
+            const newBoard = getEmptyBoard();
+            setBoard(getEmptyBoard());
+            if (mode === "vsAI" && currChoice === "X") {
+              getBestMove(newBoard, setBoard, currChoice);
+            }
+          }}
+        >
+          Restart
+        </button>
       </div>
     </div>
   );
 }
 
 interface BoxProps {
-  content: Choice;
+  content: Square;
   idx: number;
   board: Board;
   setBoard: (board: Board) => void;
+  currChoice: Choice;
+  setCurrChoice: (currChoice: Choice) => void;
+  mode: Mode;
 }
 
-function Box({ content, idx, board, setBoard }: BoxProps) {
+function Box({
+  content,
+  idx,
+  board,
+  setBoard,
+  currChoice,
+  setCurrChoice,
+  mode,
+}: BoxProps) {
   return (
-    <div onClick={() => game(idx, board, setBoard)} className={styles.box}>
+    <div
+      onClick={() =>
+        placeMark(idx, board, setBoard, currChoice, setCurrChoice, mode)
+      }
+      id={idx.toString()}
+      className={styles.box}
+    >
       {content}
     </div>
   );
